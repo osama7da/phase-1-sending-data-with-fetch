61f33ded81b97b356388e736369b61f2c2023fbc
Osama daoud <osamadaoud66@gmail.com>
2024-02-17 14:57:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+    })
+      .then((res) => res.json())
+     .then((data) => document.querySelector("body").append(data.id))
+      .catch((message) => document.querySelector("body").append(message));
+  }
